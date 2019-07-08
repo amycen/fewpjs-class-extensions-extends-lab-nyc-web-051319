063f4c5b749e22348e76ce20b4a748e860670a24
Amy Cen <amy.cen@blackrock.com>
2019-07-08 03:53:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,5 +14,7 @@ class Polygon {
 }
 
 class Triangle extends Polygon {
-
+  get isValid {
+    return (this.sides[0] + this.sides[1] > this.sides[2] && this.sides[0] + this.sides[2] > this.sides[1] && this.sides[1] + this.sides[2] > this.sides[0])
+  }
 }
