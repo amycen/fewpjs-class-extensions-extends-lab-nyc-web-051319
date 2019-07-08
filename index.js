e9b7ade1ef7bfc8f4b47015c32e24ee2f7b22e4c
Amy Cen <amy.cen@blackrock.com>
2019-07-08 03:55:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,4 +23,8 @@ class Square extends Polygon {
   get isValid {
     return (this.sides[0] === this.sides[1] && this.sides[1] === this.sides[2] && this.sides[2] === this.sides[3] && this.sides[3] === this.sides[0])
   }
+
+  get area {
+    return this.sides[0] * this.sides[0]
+  }
 }
